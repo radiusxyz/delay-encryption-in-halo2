61f12e337f7193b4d93cc82ffd66f1d10f846df2
0-knight <zeroknight.eth@gmail.com>
2023-07-26 17:57:52 +0900

    add gadgets for sinsemilla and some utilities

---
+++ src/poseidon/gadget/mod.rs
@@ -0,0 +1,6 @@
+mod primitives;
+mod pow5;
+pub mod poseidon;
+pub mod utilities;
+
+pub mod sinsemilla;
--- src/poseidon/mod.rs
+++ src/poseidon/mod.rs
@@ -2,7 +2,7 @@
 //! crate also exposes constant parameters for circuit implementations
 
 #![deny(missing_debug_implementations)]
-#![deny(missing_docs)]
+//-- #![deny(missing_docs)]
 
 mod grain;
 mod matrix;
@@ -10,5 +10,9 @@ mod permutation;
 mod poseidon;
 mod spec;
 
+mod gadget;
+pub use gadget::sinsemilla;
+
 pub use poseidon::Poseidon;
 pub use spec::{MDSMatrices, MDSMatrix, SparseMDSMatrix, Spec, State};
+
